66b3e0083b6930e746bb6e1f692fb562718f7761
sunmin <593691695@qq.com>
2017-02-08 12:14:53 +0800

    add_getua

---
+++ GetUA.js
@@ -0,0 +1,7 @@
+﻿
+  function whatBrowser() {  
+        document.Browser.Name.value=navigator.appName;  
+        document.Browser.Version.value=navigator.appVersion;  
+        document.Browser.Code.value=navigator.appCodeName;  
+        document.Browser.Agent.value=navigator.userAgent;  
+    }  
